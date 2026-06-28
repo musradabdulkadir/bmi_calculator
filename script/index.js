c8a0d814f79a37c0c2cab5280bb0fe638acc4ae5
Musrad <musradabdul007@gmail.com>
2026-06-28 15:55:17 +0530

    last dance

--- script/index.js
+++ script/index.js
@@ -1,36 +1,35 @@
 const calculate = document.getElementById("calculateButton");
 const result = document.getElementById("bmiResult");
 const category = document.getElementById("category");
+const heightInp = document.getElementById("heightInp");
+const weightInp = document.getElementById("weightInp");
 
 const hError = document.getElementById("heightError");
 const wError = document.getElementById("weightError");
 
+heightInp.addEventListener("input", function () {
+  hError.innerText = "";
+  wError.innerText = "";
+});
+
+weightInp.addEventListener("input", function () {
+  hError.innerText = "";
+  wError.innerText = "";
+});
+
 calculate.addEventListener("click", function () {
-    const heightInp = document.getElementById("heightInp")
-    const weightInp = document.getElementById("weightInp")
   const height = document.getElementById("heightInp").value;
   const weight = document.getElementById("weightInp").value;
-
-  heightInp.addEventListener("input", function(){
-        hError.innerText= ""
-        wError.innerText= ""
-    })
-    
-  weightInp.addEventListener("input", function(){
-        hError.innerText= ""
-        wError.innerText= ""
-    })
 
   if (height == "") {
     hError.style.color = "red";
     hError.innerText = "Please enter your height";
   }
-  
+
   if (weight == "") {
     wError.style.color = "red";
     wError.innerText = "Please enter your Weight";
   } else {
-
     let heightInMeters = height / 100;
     let bmi = weight / (heightInMeters * heightInMeters);
     let actualBmi = bmi.toFixed(2);
